f26072ea66706faee9dac46b57cf65e5e832a528
yashril19219 <yash1.aggarwal2310@gmail.com>
2023-11-02 17:10:35 +0530

    Created models

---
+++ JMS_Backend/models/job.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const jobSchema= new Schema({
+    tittle:{
+        type: String,
+        required: true,
+    },
+    description:{
+        type: String,
+        required: true
+    },
+    status:{
+        type: String,
+        enum:['Approved','Pending','Rejected'],
+        default:'Pending',
+        required: true
+    }
+})
+
+const Job=mongoose.model('Job',jobSchema);
+
+
+module.exports=Job;
