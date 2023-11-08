813faa79b4eea3660f5a3ab8874198c2512ac7d4
rahul1shyam <114rahulshyam@gmail.com>
2023-11-08 16:35:45 +0530

    working good, rabbitmq

--- JMS_Backend/connections/rabbitmq.js
+++ JMS_Backend/connections/rabbitmq.js
@@ -1,18 +1,9 @@
-const amqp = require('amqplib');
+const amqp=require('amqplib');
 
-async function publishMessage(message) {
-  try {
-    const connection = await amqp.connect('amqp://localhost');
-    const channel = await connection.createChannel();
-    const queueName = "queue1";
-
-    await channel.assertQueue(queueName, { durable: false });
-    channel.sendToQueue(queueName, Buffer.from(JSON.stringify(message)));
-
-    console.log(`Sent: ${message}`);
-  } catch (error) {
-    console.error(error);
-  }
+async function connectRabbitmq(url){
+    return amqp.connect(url);
 }
 
-module.exports = publishMessage;
+module.exports={
+    connectRabbitmq,
+}
